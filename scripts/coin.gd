3a5b358482c5624e7cea89ecc524b20d0f2bc705
Happyface <96100905+xHappyface@users.noreply.github.com>
2025-02-07 18:10:12 -0500

    fixed coin tossing scene

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,14 +1,10 @@
 extends Path3D
-class_name CoinToss
+class_name Coin
 
 @onready var coin: Area3D = $PathFollow3D/Coin
 @onready var toss_point: PathFollow3D = $PathFollow3D
-@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 var player: Player = null
-
-func _ready() -> void:
-	player = get_parent().get_parent().find_child("Player")
 
 func _physics_process(delta: float) -> void:
 	if player and coin.overlaps_body(player):
--- scripts/level.gd
+++ scripts/level.gd
@@ -1,16 +1,20 @@
 extends Node3D
 class_name Level
 
+@onready var coin_toss_scene: PackedScene = preload("res://scene/coin_toss.tscn")
 @onready var arena: StaticBody3D = $Arena
+@onready var tosser: Node3D = $Tosser
+@onready var player: Player = $Player
 
 func _on_coin_spawner_timeout() -> void:
-	var coin_toss_scene: PackedScene = preload("res://scene/coin_toss.tscn")
 	var arena_mesh: MeshInstance3D = arena.find_child("MeshInstance3D")
 	var spawn_point: Vector3 = Vector3(randf_range(-arena_mesh.mesh.size.x / 2.0, arena_mesh.mesh.size.x / 2.0), \
 	  0.0, randf_range(-arena_mesh.mesh.size.y / 2.0, arena_mesh.mesh.size.y / 2.0))
 	if coin_toss_scene.can_instantiate():
 		var coin_toss: Path3D = coin_toss_scene.instantiate()
-		$Tosser.add_child(coin_toss)
-		coin_toss.curve.set("point_1/position", Vector3(-$Tosser.global_position) + spawn_point)
+		tosser.add_child(coin_toss)
+		coin_toss.position = tosser.position
+		coin_toss.curve.set("point_1/position", -tosser.position + spawn_point)
 		print("spawn: %s\ntarget: %s\n" % [coin_toss.global_position, \
 		  coin_toss.curve.get("point_1/position")])
+		coin_toss.player = player
