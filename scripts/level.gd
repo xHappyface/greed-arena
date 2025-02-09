f0ea1abdc1ca30c43f58cda886492a8310ae8c27
Mae Ponce <197519630+m-1-a@users.noreply.github.com>
2025-02-09 15:10:47 -0500

    added bomb toss scene and gave it animations. bombs not yet implemented though

--- scripts/level.gd
+++ scripts/level.gd
@@ -2,9 +2,9 @@ extends Node3D
 class_name Level
 
 @onready var arena: StaticBody3D = $Arena
-@onready var tosser: Node3D = $Tosser
 @onready var player: Player = $Player
 @onready var game_timer: Timer = $GameTimer
+@onready var tossers: Node = $Tossers
 
 var level_manager: LevelManager = null
 
@@ -15,6 +15,7 @@ func _on_coin_spawner_timeout() -> void:
 	var spawn_point: Vector3 = Vector3(randf_range(-arena_mesh.mesh.size.x / 2.0, arena_mesh.mesh.size.x / 2.0), \
 	  0.0, randf_range(-arena_mesh.mesh.size.y / 2.0, arena_mesh.mesh.size.y / 2.0))
 	var toss: Toss = Toss.create_new(Toss.random_toss_object())
+	var tosser: Node3D = tossers.get_children().pick_random()
 	tosser.add_child(toss)
 	toss.position = tosser.position
 	toss.curve.set("point_1/position", -tosser.position + spawn_point)
