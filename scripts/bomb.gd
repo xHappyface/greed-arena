c895e232756081c1ef3a4d7876bccdba73022ccb
Happyface <96100905+xHappyface@users.noreply.github.com>
2025-02-19 03:32:04 -0500

    implemented ropes and bombs bouncing off ropes. also added a third money and bomb spawner. also made it so things don't get tossed inside the ropes. possibly other minor changes

--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -6,6 +6,11 @@ class_name Bomb
 @onready var shockwave: Area3D = $Shockwave
 @onready var blast: Area3D = $Blast
 
+func _ready() -> void:
+	var tween: Tween = create_tween()
+	tween.tween_callback(set_collision_layer_value.bind(6, true)).set_delay(1.0)
+	tween.tween_callback(set_collision_mask_value.bind(6, true))
+
 func _explode() -> void:
 	var shockwave_bodies: Array[Node3D] = shockwave.get_overlapping_bodies()
 	var blast_bodies: Array[Node3D] = blast.get_overlapping_bodies()
@@ -22,5 +27,5 @@ func _explode() -> void:
 			var shockwave_radius: float = shockwave.get_child(0).shape.radius
 			var distance_away: float = \
 			  (shockwave_radius - global_position.distance_to(body.global_position)) / shockwave_radius
-			body.apply_impulse((body.global_position - global_position).normalized() * 225.0 * \
+			body.apply_impulse((body.global_position - global_position).normalized() * 250.0 * \
 			  shock_curve.sample(distance_away))
--- scripts/level.gd
+++ scripts/level.gd
@@ -3,8 +3,10 @@ class_name Level
 
 @export var money_spawner_curve: Curve
 @export var money_spawner2_curve: Curve
+@export var money_spawner3_curve: Curve
 @export var bomb_spawner_curve: Curve
 @export var bomb_spawner2_curve: Curve
+@export var bomb_spawner3_curve: Curve
 
 @onready var bomb_scene: PackedScene = preload("res://scene/bomb_toss.tscn")
 
@@ -13,8 +15,10 @@ class_name Level
 @onready var game_timer: Timer = $Timers/GameTimer
 @onready var money_spawner: Timer = $Timers/MoneySpawner
 @onready var money_spawner2: Timer = $Timers/MoneySpawner2
+@onready var money_spawner3: Timer = $Timers/MoneySpawner3
 @onready var bomb_spawner: Timer = $Timers/BombSpawner
 @onready var bomb_spawner2: Timer = $Timers/BombSpawner2
+@onready var bomb_spawner3: Timer = $Timers/BombSpawner3
 @onready var player: Player = $Player
 @onready var camera: Camera3D = $Camera3D
 @onready var ground_marker: Node3D = $GroundMarker
@@ -27,8 +31,12 @@ var level_manager: LevelManager = null
 func _physics_process(_delta: float) -> void:
 	if money_spawner2.is_stopped() and game_timer.time_left <= game_timer.wait_time - 30.0:
 		money_spawner2.start()
+	if money_spawner3.is_stopped() and game_timer.time_left <= game_timer.wait_time - 75.0:
+		money_spawner3.start()
 	if bomb_spawner2.is_stopped() and game_timer.time_left <= game_timer.wait_time - 15.0:
 		bomb_spawner2.start()
+	if bomb_spawner3.is_stopped() and game_timer.time_left <= game_timer.wait_time - 60.0:
+		bomb_spawner3.start()
 
 func _on_money_spawner_timeout() -> void:
 	money_spawner.wait_time = money_spawner_curve.sample(game_timer.time_left / game_timer.wait_time)
@@ -36,9 +44,16 @@ func _on_money_spawner_timeout() -> void:
 	_spawn_at_random_tosser(toss)
 	toss.player = player
 	toss.toss_object.connect("tree_exited", level_manager.ui.update_money)
-	
+
 func _on_money_spawner2_timeout() -> void:
 	money_spawner2.wait_time = money_spawner2_curve.sample(game_timer.time_left / game_timer.wait_time)
+	var toss: Toss = Toss.create_new(Toss.random_toss_object())
+	_spawn_at_random_tosser(toss)
+	toss.player = player
+	toss.toss_object.connect("tree_exited", level_manager.ui.update_money)
+
+func _on_money_spawner3_timeout() -> void:
+	money_spawner3.wait_time = money_spawner3_curve.sample(game_timer.time_left / game_timer.wait_time)
 	var toss: Toss = Toss.create_new(Toss.random_toss_object())
 	_spawn_at_random_tosser(toss)
 	toss.player = player
@@ -54,6 +69,11 @@ func _on_bomb_spawner2_timeout() -> void:
 	var bomb: BombToss = bomb_scene.instantiate()
 	_spawn_at_random_tosser(bomb)
 
+func _on_bomb_spawner3_timeout() -> void:
+	bomb_spawner3.wait_time = bomb_spawner3_curve.sample(game_timer.time_left / game_timer.wait_time)
+	var bomb: BombToss = bomb_scene.instantiate()
+	_spawn_at_random_tosser(bomb)
+
 func _spawn_at_random_tosser(toss_scene_instance: Path3D) -> void:
 	if not level_manager:
 		return
@@ -64,6 +84,6 @@ func _spawn_at_random_tosser(toss_scene_instance: Path3D) -> void:
 	  toss_scene_instance.curve.get("point_1/position")])
 
 func _random_drop_point() -> Vector3:
-	var half_arena_mesh_side: float = arena_mesh.mesh.size.x / 2.0
+	var half_arena_mesh_side: float = (arena_mesh.mesh.size.x / 2.0) - 1.0
 	return Vector3(randf_range(-half_arena_mesh_side, half_arena_mesh_side), \
 	  0.01, randf_range(-half_arena_mesh_side, half_arena_mesh_side))
