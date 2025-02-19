969c68f9ca732b8e3a5e71a611dc822e91190c6c
Happyface <96100905+xHappyface@users.noreply.github.com>
2025-02-19 13:31:53 -0500

    i have tried several times previously to remove a bug where bombs fall through the arena. this has not been documented as the changes have been seen as minor ones. however, this is a brute force attempt at fixing said bug in its own dedicated commit.

--- scripts/bomb.gd
+++ scripts/bomb.gd
@@ -6,10 +6,17 @@ class_name Bomb
 @onready var shockwave: Area3D = $Shockwave
 @onready var blast: Area3D = $Blast
 
+var is_y_snapped: bool = false
+
 func _ready() -> void:
 	var tween: Tween = create_tween()
 	tween.tween_callback(set_collision_layer_value.bind(6, true)).set_delay(1.0)
 	tween.tween_callback(set_collision_mask_value.bind(6, true))
+	tween.tween_callback(set.bind("is_y_snapped", true))
+
+func _physics_process(_delta: float) -> void:
+	if is_y_snapped:
+		global_position.y = 0.01
 
 func _explode() -> void:
 	var shockwave_bodies: Array[Node3D] = shockwave.get_overlapping_bodies()
