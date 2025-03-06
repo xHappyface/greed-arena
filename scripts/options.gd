091ade3d38db50b23a25ee75215208650909d614
Happyface <96100905+xHappyface@users.noreply.github.com>
2025-03-06 03:16:11 -0500

    implemented yarn ball as player with movement based rolling. also implemented keyboard and gamepad control though they do not work in the menus as of yet.

--- scripts/options.gd
+++ scripts/options.gd
@@ -3,6 +3,11 @@ class_name OptionsMenu
 
 @onready var audio_volume_slider: HSlider = $VBoxContainer/Volume/HSlider
 @onready var audio_mute_checkbox: CustomCheckbox = $VBoxContainer/Mute/CustomCheckbox
+@onready var resolution_option_button: OptionButton = $VBoxContainer/Resolution/OptionButton
+
+
+static func set_input_controls(input_control: int) -> void:
+	Player.input_controls = input_control
 
 static func set_volume(percent: float) -> void:
 	var db: float = -55.0 + ((55.0 / 100.0) * percent)
@@ -11,10 +16,12 @@ static func set_volume(percent: float) -> void:
 static func set_mute(state: bool) -> void:
 	AudioServer.set_bus_mute(0, state)
 
-static func set_fullscreen(state: bool) -> void:
+func set_fullscreen(state: bool) -> void:
 	if state:
+		resolution_option_button.set_disabled(true)
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
+		resolution_option_button.set_disabled(false)
 		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func save_options_and_go_back() -> void:
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,19 @@
 extends CharacterBody3D
 class_name Player
 
+@onready var yarn_ball: Node3D = $YarnBall
 @onready var nav: NavigationAgent3D = $NavigationAgent3D
 @onready var magnetic_field: Area3D = $MagneticField
 @onready var shield: Node3D = $Shield
 @onready var shield_timer: Timer = $Shield/Timer
+
+enum InputControls {
+	MOUSE,
+	KEYBOARD,
+	GAMEPAD,
+}
+
+static var input_controls: InputControls = InputControls.MOUSE
 
 var camera: Camera3D = null
 var camera_ray: RayCast3D = null
@@ -25,16 +34,29 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	shield.global_position = Vector3(global_position.x, 0.6, global_position.z)
-	if nav.is_navigation_finished():
-		if LevelProvider.level.ground_marker.visible:
-			LevelProvider.level.ground_marker.hide()
-		return
-	else:
-		look_at(nav.target_position)
-		velocity = delta * global_position.direction_to(nav.target_position) * speed
+	match input_controls:
+		InputControls.KEYBOARD:
+			_handle_keyboard_movement(delta)
+			if Input.is_action_just_pressed("keyboard_action"):
+				_use_item()
+		InputControls.GAMEPAD:
+			_handle_gamepad_movement(delta)
+			if Input.is_action_just_pressed("gamepad_action"):
+				_use_item()
+		_:
+			if nav.is_navigation_finished():
+				if LevelProvider.level.ground_marker.visible:
+					LevelProvider.level.ground_marker.hide()
+				return
+			else:
+				_handle_mouse_movement(delta)
+	if not (velocity == Vector3.ZERO):
+		yarn_ball.rotation_degrees.x -= Vector3.ZERO.distance_to(velocity) * 0.5
 	move_and_slide()
 
 func _unhandled_input(event: InputEvent) -> void:
+	if not input_controls == InputControls.MOUSE:
+		return
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
 		var mouse_pos: Vector2 = event.position
 		camera_ray.global_position = camera.project_ray_origin(mouse_pos)
@@ -47,22 +69,7 @@ func _unhandled_input(event: InputEvent) -> void:
 			if not LevelProvider.level.ground_marker.visible:
 				LevelProvider.level.ground_marker.show()
 	elif event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT and event.is_pressed():
-		if held_items.size() > 0:
-			var item: Toss.TossObject = held_items.pop_front()
-			if LevelProvider.level.level_manager.ui.held_item_slot2.visible:
-				LevelProvider.level.level_manager.ui.held_item_slot2.hide()
-				LevelProvider.level.level_manager.ui.held_item_slot1.texture = \
-				  LevelProvider.level.level_manager.ui.held_item_slot2.texture
-			else:
-				LevelProvider.level.level_manager.ui.held_item_slot1.hide()
-			match item:
-				Toss.TossObject.SHIELD:
-					shield_timer.stop()
-					if not shield.visible:
-						shield.show()
-					shield_timer.start()
-				_:
-					LevelProvider.slow_time()
+		_use_item()
 
 func take_hit() -> void:
 	if shield.visible:
@@ -92,6 +99,40 @@ func set_player_magnetism(rank: int) -> void:
 		_:
 			magnetic_field.monitoring = false
 			magnetic_field.visible = false
+
+func _handle_mouse_movement(delta: float) -> void:
+	look_at(nav.target_position)
+	velocity = delta * global_position.direction_to(nav.target_position) * speed
+
+func _handle_keyboard_movement(delta: float) -> void:
+	var move_dir: Vector2 = Input.get_vector("keyboard_left", "keyboard_right", "keyboard_down", "keyboard_up", 0.25).normalized()
+	if not move_dir.is_zero_approx():
+		look_at(global_position + Vector3(move_dir.y, 0.0, move_dir.x))
+		velocity = delta * Vector3(move_dir.y, 0.0, move_dir.x) * speed
+
+func _handle_gamepad_movement(delta: float) -> void:
+	var move_dir: Vector2 = Input.get_vector("gamepad_left", "gamepad_right", "gamepad_down", "gamepad_up", 0.25).normalized()
+	if not move_dir.is_zero_approx():
+		look_at(global_position + Vector3(move_dir.y, 0.0, move_dir.x))
+		velocity = delta * Vector3(move_dir.y, 0.0, move_dir.x) * speed
+
+func _use_item() -> void:
+	if held_items.size() > 0:
+		var item: Toss.TossObject = held_items.pop_front()
+		if LevelProvider.level.level_manager.ui.held_item_slot2.visible:
+			LevelProvider.level.level_manager.ui.held_item_slot2.hide()
+			LevelProvider.level.level_manager.ui.held_item_slot1.texture = \
+			  LevelProvider.level.level_manager.ui.held_item_slot2.texture
+		else:
+			LevelProvider.level.level_manager.ui.held_item_slot1.hide()
+		match item:
+			Toss.TossObject.SHIELD:
+				shield_timer.stop()
+				if not shield.visible:
+					shield.show()
+				shield_timer.start()
+			_:
+				LevelProvider.slow_time()
 
 func _on_magnetic_field_area_entered(area: Area3D) -> void:
 	if area is Money:
